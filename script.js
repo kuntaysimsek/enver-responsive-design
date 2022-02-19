8ade08341de5c268ef56cf3b55436f4346e4258a
YalcinKuntaySimsek <kuntayyalcin.simsek@gmail.com>
2022-02-19 20:29:15 +0300

    mobil fixed

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const playIcon = document.getElementsByClassName("video-player-icon");
 const player = document.getElementsByClassName("video-box");
 const playIconWidth = playIcon[0].clientWidth;
 const playIconHeight = playIcon[0].clientHeight;
-
+const body = document.body;
 let animation = true;
+
 
 let x = 0;
 let y = 0;
@@ -45,47 +46,52 @@ function playerAnimationEnd() {
   animation = false;
 }
 
+window.onload = function(){
+  returnCenterPlayIcon();
+ }
+
 function returnCenterPlayIcon() {
   x = (player[0].clientWidth / 2) - (playIconWidth / 2);
   y = (player[0].clientHeight / 2) - (playIconHeight / 2);
 }
 
+
 // slider //
 
-const slider = document.getElementsByClassName("slider-items");
-const sliderItem = document.getElementsByClassName("slider-item");
-const sliderItemWidth = sliderItem[0].clientWidth;
+// const slider = document.getElementsByClassName("slider-items");
+// const sliderItem = document.getElementsByClassName("slider-item");
+// const sliderItemWidth = sliderItem[0].clientWidth;
 
-let startx = 0;
-let sliderX = 0;
-let dragScrollLeft = 0;
-let pressed = false;
+// let startx = 0;
+// let sliderX = 0;
+// let dragScrollLeft = 0;
+// let pressed = false;
 
-function mouseDown(e) {
-  if (e.which == 0) {
-    startx = e.touches[0].pageX - slider[0].offsetLeft;
-  } else {
-    startx = e.pageX - slider[0].offsetLeft;
-  }
-  pressed = true;
-  dragScrollLeft = slider[0].scrollLeft;
-}
-function mouseUp() {
-  pressed = false;
-}
-function mouseLeave() {
-  pressed = false;
-}
-function mouseMove(e) {
-    if (!pressed) return;
-    if (e.which == 0) {
-      sliderX = e.touches[0].pageX - slider[0].offsetLeft;
-    } else {
-      sliderX = e.pageX - slider[0].offsetLeft;
-    }
-    const walk = sliderX - startx;
-    slider[0].scrollLeft = dragScrollLeft - walk; 
-}
+// function mouseDown(e) {
+//   if (e.which == 0) {
+//     startx = e.touches[0].pageX - slider[0].offsetLeft;
+//   } else {
+//     startx = e.pageX - slider[0].offsetLeft;
+//   }
+//   pressed = true;
+//   dragScrollLeft = slider[0].scrollLeft;
+// }
+// function mouseUp() {
+//   pressed = false;
+// }
+// function mouseLeave() {
+//   pressed = false;
+// }
+// function mouseMove(e) {
+//     if (!pressed) return;
+//     if (e.which == 0) {
+//       sliderX = e.touches[0].pageX - slider[0].offsetLeft;
+//     } else {
+//       sliderX = e.pageX - slider[0].offsetLeft;
+//     }
+//     const walk = sliderX - startx;
+//     slider[0].scrollLeft = dragScrollLeft - walk; 
+// }
 
 const btn = document.querySelector(".nav-menu-button");
 
