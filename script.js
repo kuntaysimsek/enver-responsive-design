ef6986ccf496cc98562f2dfe89d18d0e31e547c4
YalcinKuntaySimsek <kuntayyalcin.simsek@gmail.com>
2022-02-22 07:55:49 +0300

    update mobil menu button

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ function playerAnimationEnd() {
   animation = false;
 }
 
-window.onload = function(){
+body.onload = function(){
   returnCenterPlayIcon();
  }
 
@@ -55,48 +55,10 @@ function returnCenterPlayIcon() {
   y = (player[0].clientHeight / 2) - (playIconHeight / 2);
 }
 
-
-// slider //
-
-// const slider = document.getElementsByClassName("slider-items");
-// const sliderItem = document.getElementsByClassName("slider-item");
-// const sliderItemWidth = sliderItem[0].clientWidth;
-
-// let startx = 0;
-// let sliderX = 0;
-// let dragScrollLeft = 0;
-// let pressed = false;
-
-// function mouseDown(e) {
-//   if (e.which == 0) {
-//     startx = e.touches[0].pageX - slider[0].offsetLeft;
-//   } else {
-//     startx = e.pageX - slider[0].offsetLeft;
-//   }
-//   pressed = true;
-//   dragScrollLeft = slider[0].scrollLeft;
-// }
-// function mouseUp() {
-//   pressed = false;
-// }
-// function mouseLeave() {
-//   pressed = false;
-// }
-// function mouseMove(e) {
-//     if (!pressed) return;
-//     if (e.which == 0) {
-//       sliderX = e.touches[0].pageX - slider[0].offsetLeft;
-//     } else {
-//       sliderX = e.pageX - slider[0].offsetLeft;
-//     }
-//     const walk = sliderX - startx;
-//     slider[0].scrollLeft = dragScrollLeft - walk; 
-// }
-
 const btn = document.querySelector(".nav-menu-button");
 
 btn.addEventListener("click", () => {
-  console.log("tik");
+  document.body.classList.toggle("scroll-block");
   document.querySelector(".box").classList.toggle("open");
   document.querySelector(".rectangle--top").classList.toggle("open");
   document.querySelector(".rectangle--middle").classList.toggle("open");
